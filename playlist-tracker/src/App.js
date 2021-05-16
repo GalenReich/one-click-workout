98fbec92b40abddc4801c00fa0df6778b9dd8274
Galen Reich <galen@reich.me.uk>
2021-05-16 14:00:29 +0100

    lift video control out of youtubeembed and add simple playlist logic

--- playlist-tracker/src/App.js
+++ playlist-tracker/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -28,9 +28,12 @@ const theme = createMuiTheme({
 });
 
 function ProgressView(props) {
-  const timeout = 10000 //10 seconds
+  const timeout = 3000 //3 seconds
   setTimeout(() => {
-    window.location.href = "/video";
+    console.log(window.location)
+    if (window.location.pathname == "/") {
+      window.location.href = "/video";
+    }
   }, timeout);
   return (
     <ThemeProvider theme={theme}>
@@ -52,7 +55,7 @@ function VideoView(props) {
         <LinearProgress variant="determinate" value={props.progress} />
       </div>
       <div className="Youtube">
-        <YoutubeEmbed embedId="JRvLn-A_2dM" />
+        <YoutubeEmbed embedId={props.embedId} setVideoFinished={props.setVideoFinished} />
       </div>
     </ThemeProvider>
   );
@@ -67,23 +70,50 @@ function SummaryView() {
   );
 }
 
+function doNextThing(setEmbedId, videoIdx, setVideoIdx, videoFinished, setVideoFinished) {
+
+  const videoObj = {
+    1: "jhsf1a4XG-U",
+    2: "ypDwh2ShJ6A"
+  }
+
+  if (videoFinished === true) {
+    videoIdx = videoIdx + 1;
+    if (videoIdx in videoObj) {
+      setVideoIdx(videoIdx);
+      setEmbedId(videoObj[videoIdx]);
+      setVideoFinished(false);
+    } else {
+      window.location.href = "/summary";
+    }
+
+  }
+}
+
 function App() {
 
   const [progress, setProgress] = useState(5.0);
+  const [embedId, setEmbedId] = useState("JRvLn-A_2dM");
+  const [videoFinished, setVideoFinished] = useState(true);
+  const [videoIdx, setVideoIdx] = useState(0);
+
+  useEffect(() => {
+    doNextThing(setEmbedId, videoIdx, setVideoIdx, videoFinished, setVideoFinished);
+  }, [videoFinished])
 
   return (
     <Router>
       <div>
         <Switch>
           <Route path="/summary" component={() => <SummaryView />} />
-          <Route path="/video" component={() => <VideoView progress={progress} />} />
+          <Route path="/video" component={() => <VideoView progress={progress} embedId={embedId} setVideoFinished={setVideoFinished} />} />
           <Route path="/" component={() => <ProgressView progress={progress} />} />
         </Switch>
 
         <nav>
-              <Link to="/">Progress</Link>
-              <Link to="/video">Video</Link>
-              <Link to="/summary">Summary</Link>
+          <Link to="/">Progress</Link>
+          <Link to="/video">Video</Link>
+          <Link to="/summary">Summary</Link>
         </nav>
       </div>
     </Router >
--- playlist-tracker/src/YoutubeEmbed.js
+++ playlist-tracker/src/YoutubeEmbed.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import YouTube from "react-youtube";
 
 
 function YoutubeEmbed(props) {
+
+    const embedId = props.embedId;
+    const setVideoFinished = props.setVideoFinished;
 
     const playerOpts = {
         playerVars: {
@@ -14,39 +17,17 @@ function YoutubeEmbed(props) {
         }
     }
 
-    const [embedId, setEmbedId] = useState();
-
-    useEffect(() => {
-        setEmbedId(props.embedId)
-    }, [props.embedId]);
-
     return (
         <div className="video-responsive" >
             <YouTube
                 videoId={embedId}
                 containerClassName="embed embed-youtube"
-                onStateChange={(e) => checkElapsedTime(setEmbedId, e)}
+                onEnd={(e) => setVideoFinished(true)}
                 opts={playerOpts}
             />
         </div >
     );
 
 }
-
-function checkElapsedTime(setEmbedId, e) {
-    console.log(e.target.playerInfo.playerState);
-    const duration = e.target.getDuration();
-    const currentTime = e.target.getCurrentTime();
-    if (currentTime / duration > 0.95) {
-        console.log(currentTime / duration);
-        changeVideo(setEmbedId, "2uXS20iWve4")
-    }
-};
-
-function changeVideo(setEmbedId, newEmbedId) {
-    setEmbedId(newEmbedId);
-}
-
-
 
 export default YoutubeEmbed;
